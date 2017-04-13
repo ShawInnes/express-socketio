2d77ff58d61165fb99468069e7cd3553b7fd390d
Shaw Innes <shaw.innes@tattsgroup.com>
2017-04-13 18:55:34 +1000

    Add Authentication to Socket.io

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var moment = require('moment');
+// var redisadapter = require('socket.io-redis');
+var socketioJwt = require('socketio-jwt');
 
-server.listen(8000);
+// io.adapter(redisadapter({ host: 'localhost', port: 6379 }));
 
 var list = [ 1,2,3,4,5,6,7,8 ];
 
@@ -12,7 +14,7 @@ setInterval(function () {
     io.emit('news', {
       index: i,
       type: 'update',
-      time: moment()
+      time: moment().toString()
     });
   });
 }, 1000);
@@ -27,8 +29,15 @@ app.get('/send/:pair', function (req, res) {
   res.send('ok');
 });
 
-io.on('connect', function (socket) {
-  socket.on('news', function (data) {
-    console.log('news', data);
+io
+  .on('connection', socketioJwt.authorize({
+    secret: 'secret',
+    timeout: 15000 // 15 seconds to send the authentication message
+  })).on('authenticated', function(socket) {
+    //this socket is authenticated, we are good to handle more events from it.
+    console.log('hello!', socket.decoded_token);
   });
+
+server.listen(8000, function (port) {
+  console.log('listening');
 });
